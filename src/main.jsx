c20978d58a2f6a2bab6be796abf9ead49b82f692
abdoyasser4 <abdoy83214@gmail.com>
2024-10-30 23:00:40 +0300

    added home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import Home from "./wepsite/Home.jsx";
+import { Route, Router } from "react-router-dom";
 import Navbar from "./wepsite/Nav.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App />
-    <Home /> */}
     <Navbar />
+    <Home />
+    {/* <Router>
+      <Route path="/" element={<Home />} />
+    </Router> */}
   </StrictMode>
 );
--- src/wepsite/Home.jsx
+++ src/wepsite/Home.jsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <p>holaaa</p>
+    <div class="container">
+      <h2>lorem ipsum?</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur. Viverra amet blandit lorem
+        commodo id cursus proin sagittis. Velit orci tortor pellentesque
+        fringilla. Ac mus fermentum magna phasellus iaculis. Adipiscing nibh
+        faucibus ullamcorper gravida mattis pharetra in.
+      </p>
+      <button class="get-started">get started</button>
     </div>
   );
 }
